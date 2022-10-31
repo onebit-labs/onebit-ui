4dd577ec375cc25961694b798ba707038e866334
rockgold0911 <rockgold0911@gmail.com>
2022-10-31 10:57:13 +0800

    style: portfolioDetails depositors init

--- UI/portfolioDetails/Depositors.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react'
-
-const Depositors: FC = () => {
-  return <></>
-}
-
-export default Depositors
---
+++ UI/portfolioDetails/Depositors/useTable.tsx
@@ -0,0 +1,57 @@
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { cellRenderer, headerRenderer } from 'components/table/renderer'
+import type { TableColumnsProps, BasicTableProps } from 'components/table/BasicTable/types'
+
+export const useTable = (): BasicTableProps => {
+  const { t } = useTranslation('portfolioDetails')
+
+  const columns = useMemo(
+    () =>
+      (
+        [
+          {
+            dataKey: 'title',
+            width: 250,
+            headerRenderer,
+            cellRenderer,
+          },
+          {
+            dataKey: 'depositors',
+            width: 250,
+            headerRenderer,
+            cellRenderer,
+          },
+          {
+            dataKey: 'since',
+            width: 250,
+            headerRenderer,
+            cellRenderer,
+          },
+          {
+            dataKey: 'equity',
+            width: 250,
+            headerRenderer,
+            cellRenderer,
+          },
+          {
+            dataKey: 'percentage',
+            width: 250,
+            headerRenderer,
+            cellRenderer,
+          },
+        ] as TableColumnsProps[]
+      ).map((column) => {
+        column.label = t('overview.historicalPerformance' + column.dataKey)
+        return column
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [t]
+  )
+
+  return {
+    columns,
+    data: [],
+  }
+}
